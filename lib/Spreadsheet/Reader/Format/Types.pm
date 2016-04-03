2b86f31aa583e3a8fb1cd252afa6de682b30d59f
Jed Lund <jandrew@cpan.org>
2016-04-02 20:54:00 -0700

    v0.2.0 	+ Added the badges
    Signed-off-by: Jed Lund <jandrew@cpan.org>

--- lib/Spreadsheet/Reader/Format/Types.pm
+++ lib/Spreadsheet/Reader/Format/Types.pm
@@ -1,5 +1,5 @@
 package Spreadsheet::Reader::Format::Types;
-use version; our $VERSION = version->declare('v0.1_3');
+use version; our $VERSION = version->declare('v0.2.0');
 ###LogSD	warn "You uncovered internal logging statements for Spreadsheet::Reader::Format::Types-$VERSION";
 		
 use strict;
